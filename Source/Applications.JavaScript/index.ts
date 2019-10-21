0abe1be87951fdc7c24ee3c27163ae2cda4e681b
Einar Ingebrigtsen <einar@dolittle.com>
2019-10-21 08:54:32 +0200

    Cleaning up for the new naming approach

--- Source/Applications.JavaScript/index.ts
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-import * as management from './Application/management/index';
-import * as server from './Application/server/index';
-import * as grpc from 'grpc';
-
-export { grpc, management, server };
